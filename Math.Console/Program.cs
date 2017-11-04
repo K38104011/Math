5dc5d90bed291f50ef3596cd314ce859f8c4334d
Phạm Đặng Hoàng Giang <phamdanghoanggiang@gmail.com>
2017-11-04 19:16:17 +0700

    first commit

---
+++ Math.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Math.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Math.Gcf(13));
+            System.Console.WriteLine(Math.Gcf(30, 15, 45));
+            System.Console.WriteLine(Math.Gcf(13, 11, 19));
+            System.Console.WriteLine(Math.Gcf(20, 60));
+            System.Console.WriteLine(Math.EuclidGcf(13));
+            System.Console.WriteLine(Math.EuclidGcf(30, 15, 45));
+            System.Console.WriteLine(Math.EuclidGcf(13, 11, 19));
+            System.Console.WriteLine(Math.EuclidGcf(20, 60));
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Math/GCF.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Math
+{
+    public static class Math
+    {
+        public static int Gcf(params int[] integers)
+        {
+            if (integers.Length == 1)
+            {
+                return integers[0];
+            }
+            var gfc = 1;
+            var min = integers.Min();
+            if (integers.All(integer => integer % min == 0))
+            {
+                return min;
+            }
+            var factors = GetFactors(min);
+            factors = factors.OrderByDescending(value => value).ToList();
+            foreach (var factor in factors)
+            {
+                if (integers.All(integer => integer % factor == 0))
+                {
+                    return factor;
+                }
+            }
+            return gfc;
+        }
+
+        public static ICollection<int> GetFactors(int integer)
+        {
+            var factors = new List<int>();
+            factors.AddRange(new []{ 1, integer });
+            for (var i = 2; i < integer / 2; i++)
+            {
+                if (integer % i == 0)
+                {
+                    factors.AddRange(new []{i, integer / i});
+                }
+            }
+            return factors;
+        }
+
+        public static int EuclidGcf(params int[] integers)
+        {
+            if (integers.Length == 1)
+            {
+                return integers[0];
+            }
+            var @int1 = integers.Min();
+            var @int2 = integers.Max();
+            do
+            {
+                var gfc = int1 > int2 ? int1 - int2 : int2 - int1;
+                if (gfc == 1 || gfc == int1 || gfc == int2)
+                {
+                    return gfc;
+                }
+                var unused = int1 > int2 ? int1 = gfc : int2 = gfc;
+            } while (true);
+        }
+    }
+
+    
+}
